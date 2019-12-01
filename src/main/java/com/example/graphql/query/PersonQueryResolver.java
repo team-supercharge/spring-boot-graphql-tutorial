1d05bb937beef5c9b96e965f91793a9381d3efea
Balázs Vajner <vajner.balazs@gmail.com>
2019-12-01 17:20:23 +0100

    feat: people query

---
+++ src/main/java/com/example/graphql/query/PersonQueryResolver.java
@@ -0,0 +1,23 @@
+package com.example.graphql.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+import java.util.Collection;
+
+import com.example.graphql.model.Person;
+import com.example.graphql.service.PersonService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonQueryResolver implements GraphQLQueryResolver {
+
+    private final PersonService personService;
+
+    public PersonQueryResolver(final PersonService personService) {
+        this.personService = personService;
+    }
+
+    public Collection<Person> people() {
+        return personService.getPeople();
+    }
+}

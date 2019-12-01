82045779a5aaf31c343eefbf19bd05f41ced0acc
Balázs Vajner <vajner.balazs@gmail.com>
2019-12-01 17:09:02 +0100

    feat: hello query, with optional parameter

--- src/main/java/com/example/graphql/query/HelloQueryResolver.java
+++ src/main/java/com/example/graphql/query/HelloQueryResolver.java
@@ -1,12 +1,13 @@
 package com.example.graphql.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloQueryResolver implements GraphQLQueryResolver {
 
-    public String hello() {
-        return "Hello, GraphQL!";
+    public String hello(final String who) {
+        return String.format("Hello, %s!", Optional.ofNullable(who).orElse("GraphQL"));
     }
 }
